f255b1adeb0ed161108482317edf2b2d725363c1
Thomas Taylor <jjtaylor1925@gmail.com>
2023-04-11 02:09:34 -0400

    add initial `index.astro` skeleton

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,30 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
+    container: {
+      center: true,
+      screens: {
+        sm: "500px"
+      }
+    },
 		extend: {},
 	},
-	plugins: [require("daisyui")],
+  daisyui: {
+    themes: [
+      {
+        mydark: {
+          "primary": "#d1d5db",
+          "secondary": "#d1d5db",
+          "accent": "#d1d5db",
+          "neutral": "#272626",
+          "base-100": "#000000",
+          "info": "#0000FF",
+          "success": "#008000",
+          "warning": "#FFFF00",
+          "error": "#FF0000",
+        },
+      },
+    ],
+  },
+	plugins: [require("@tailwindcss/typography"), require("daisyui")]
 }
